a9a5e232b255943c09914f3d671cf74485ed5317
Qngdjas <qngdjas@mail.ru>
2022-11-26 17:42:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab1mobile/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lab1mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val button: Button = findViewById(R.id.button);
+//        button.setOnClickListener {
+//            findViewById<TextView>(R.id.textView2).text = "Hi, " + findViewById<EditText>(R.id.editTextPersonName).text
+//        }
+    }
+}
